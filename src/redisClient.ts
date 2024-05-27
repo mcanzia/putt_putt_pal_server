8fe5fe51ca9fb2cbebad0bb9c9536aa225b02df0
Michael Canziani <canziani.michael@gmail.com>
2024-05-27 18:25:29 -0400

    commenting out redis client stuff for now

--- src/redisClient.ts
+++ src/redisClient.ts
@@ -1,43 +1,43 @@
-import { createClient } from 'redis';
-import Logger from './util/logs/logger';
-import cache from 'express-redis-cache';
-import { promisify } from 'util';
+// import { createClient } from 'redis';
+// import Logger from './util/logs/logger';
+// import cache from 'express-redis-cache';
+// import { promisify } from 'util';
 
-console.log('Connecting to Redis at:', process.env.REDIS_HOST);
-const redisClient = createClient({
-    socket: {
-        host: process.env.REDIS_HOST || 'localhost',
-        port: 6379
-    },
-  });
+// console.log('Connecting to Redis at:', process.env.REDIS_HOST);
+// const redisClient = createClient({
+//     socket: {
+//         host: process.env.REDIS_HOST || 'localhost',
+//         port: 6379
+//     },
+//   });
   
-redisClient.on('error', (err) => Logger.error('Redis Client Error', err));
+// redisClient.on('error', (err) => Logger.error('Redis Client Error', err));
 
-redisClient.connect().then(() => {
-  Logger.info('Redis client connected');
-}).catch((err) => {
-  Logger.error('Failed to connect to Redis', err);
-});
+// redisClient.connect().then(() => {
+//   Logger.info('Redis client connected');
+// }).catch((err) => {
+//   Logger.error('Failed to connect to Redis', err);
+// });
 
-const redisCache = cache({
-  client: redisClient,
-  prefix: 'putt-putt-pal'
-});
+// const redisCache = cache({
+//   client: redisClient,
+//   prefix: 'putt-putt-pal'
+// });
 
-redisCache.on('connected', () => {
-  Logger.info('Redis cache connected');
-});
+// redisCache.on('connected', () => {
+//   Logger.info('Redis cache connected');
+// });
 
-redisCache.on('disconnected', () => {
-  Logger.warn('Redis cache disconnected');
-});
+// redisCache.on('disconnected', () => {
+//   Logger.warn('Redis cache disconnected');
+// });
 
-redisCache.on('error', (error: any) => {
-  Logger.error('Redis Cache Error:', error);
-});
+// redisCache.on('error', (error: any) => {
+//   Logger.error('Redis Cache Error:', error);
+// });
 
-const getAsync = promisify(redisClient.get).bind(redisClient);
-const setAsync = promisify(redisClient.set).bind(redisClient);
-const deleteAsync = promisify(redisClient.del).bind(redisClient);
+// const getAsync = promisify(redisClient.get).bind(redisClient);
+// const setAsync = promisify(redisClient.set).bind(redisClient);
+// const deleteAsync = promisify(redisClient.del).bind(redisClient);
 
-export {redisClient, getAsync, setAsync, deleteAsync};
+// export {redisClient, getAsync, setAsync, deleteAsync};
